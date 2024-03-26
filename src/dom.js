f8e56cf67057a8b60b56951af0201264cb081882
Jack Brophy <jackbrophy5@gmail.com>
2024-03-26 17:44:07 +0100

    Create dom.js functions for adding beer menu and food menu. There is also a function for clearing the current content that will be run at the beginning of each generation function. The function to generate the "About" generator has yet to be written.

--- src/dom.js
+++ src/dom.js
@@ -1,20 +1,48 @@
 "use strict";
 
+const content = document.querySelector("#content");
+
 const clearContent = function () {
-  const content = document.querySelector("#content");
   while (content.firstChild) {
     content.removeChild(content.lastChild);
   }
 };
 
+const generateMenuDiv = function (itemString, priceString) {
+  const newDiv = document.createElement("div");
+  content.appendChild("div");
+  newDiv.classList.add("menu-item");
+
+  const item = document.createElement("div");
+  newDiv.appendChild("div");
+  item.classList.add("item");
+
+  const price = document.createElement("div");
+  newDiv.appendChild("div");
+  price.classList.add("price");
+
+  item.textContent = itemString;
+  price.textContent = priceString;
+};
+
 //const createSkeleton = function () {};
 
-const generateWine = function () {
+const generateBeer = function () {
   clearContent();
+
+  generateMenuDiv("Wiener IPA", "10$");
+  generateMenuDiv("Penile Pilsner", "8$");
+  generateMenuDiv("Heffewiener", "8$");
+  generateMenuDiv("Face Melter", "10$");
 };
 
 const generateFood = function () {
   clearContent();
+
+  generateMenuDiv("Cheese Wiener", "10$");
+  generateMenuDiv("Chilli Wiener", "10$");
+  generateMenuDiv("Chilli Cheese Wiener", "12$");
+  generateMenuDiv("Wiener Wurst", "14$");
 };
 
 const generateAbout = function () {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import "./styles.css";
 import "./dom.js";
 import hotdog from "./images/hotdog.png";
+import ketchup from "./images/ketchup.png";
 console.log("Hello, webpack!");
 
 const body = document.querySelector("body");
+const content = document.querySelector("#content");
+const nav = document.querySelector("nav");
+const title = document.createElement("div");
+
+const ketchupIcon = new Image();
+ketchupIcon.src = ketchup;
+
+// nav.addEventListener("click", function (e) {
+// for (let i of document.querySelectorAll("button")) {
+//   if (i.firstChild) {
+//     i.removeChild(ketchupIcon);
+//   }
+// }
+
+//   console.log(e.target.classList);
+
+//   if ([...e.target.classList].includes("nav-button")) {
+//     e.target.appendChild(ketchupIcon);
+//   }
+// });
